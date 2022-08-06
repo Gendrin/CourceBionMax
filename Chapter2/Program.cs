5d82bfca7c32e2719e96bf4076393a02daead678
GendrinVL <GendrinVL@gmail.com>
2022-08-06 21:01:17 +1000

    Домашнее задание №3 по второй главе.

--- Chapter2/Program.cs
+++ Chapter2/Program.cs
@@ -1,40 +1,94 @@
-﻿/*Запросить у пользователя длины трёх сторон треугольника. Длины могут быть представлены дробными значениями.
+﻿/*Практическое задание "Профиль пользователя"
+Запросить у пользователя: фамилию, имя, возраст, вес, рост.
 
-После получения длин сторон - использовать формулу Герона для вычисления площади треугольника.
+Рассчитать ИМТ(индекс массы тела) по формуле ИМТ = вес (кг) / (рост(м) * рост(м))
 
-Чтобы жизнь не казалась мёдом, найдите формулу самостоятельно.
+Сформировать единую строку, в следующем формате:
+Your profile:
+Full Name: фамилия, имя
+Age: рост
+Weight: вес
+Height: рост
+Body Mass Index: ИМТ
 
-После вычисления площади - вывести результат на консоль.*/
-Console.WriteLine("Введите длины трех сторон треугольника:");
-Console.WriteLine("Введите значение первой стороны:");
-string? astr, bstr, cstr;
-double a, b, c, p, s;
-astr = Console.ReadLine();
-if (string.IsNullOrWhiteSpace(astr)) Console.WriteLine("Значение не введено!");
-    else
+Вывести сформированную строку на консоль.*/
+string? fio,ages,weights,heights,profile="You profile:";
+int age;
+double weight, height;
+Console.WriteLine("Формируем ваш профайл:");
+Console.WriteLine("Введите фамилию и имя:");
+
+fio=Console.ReadLine();
+if (string.IsNullOrWhiteSpace(fio)) Console.WriteLine("Значение не введено!");
+else
 {
-    Console.WriteLine($"Введено значение a: {astr}");
-    Console.WriteLine("Введите значение второй стороны:");
-    bstr = Console.ReadLine();
-    if (string.IsNullOrWhiteSpace(bstr)) Console.WriteLine("Значение не введено!");
+    Console.WriteLine("Введите ваш возраст (целое):");
+    ages=Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(ages)) Console.WriteLine("Значение не введено!");
     else
     {
-        Console.WriteLine($"Введено значение b: {bstr}");
-        Console.WriteLine("Введите значение третьей стороны:");
-        cstr = Console.ReadLine();
-        if (string.IsNullOrWhiteSpace(cstr)) Console.WriteLine("Значение не введено!");
+        age=int.Parse(ages);
+        Console.WriteLine("Введите ваш вec (можно дробное):");
+        weights = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(weights)) Console.WriteLine("Значение не введено!");
         else
         {
-            Console.WriteLine($"Введено значение c: {cstr}");
-            a=Double.Parse(astr);b = Double.Parse(bstr);c = Double.Parse(cstr);
-            p = (a + b + c) / 2;
-            s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
-            Console.WriteLine($"Площадь треугольника S={s:f2}");
+            weight = Double.Parse(weights);
+            Console.WriteLine("Введите ваш рост в метрах (дробное):");
+            heights = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(heights)) Console.WriteLine("Значение не введено!");
+            else
+            {
+                height = Double.Parse(heights);
+                Console.WriteLine($"{profile}{Environment.NewLine}Full Name:{fio}{Environment.NewLine}" +
+                $"Age: {age}{Environment.NewLine}" +
+                $"Weight: {weight}{Environment.NewLine}" +
+                $"Height: {height}{Environment.NewLine}" +
+                $"Body Mass Index: {weight/(height*height)}");
+                
+            }
         }
     }
 }
 
-//DZ1();
+static void DZ2()
+{
+   /*Запросить у пользователя длины трёх сторон треугольника. Длины могут быть представлены дробными значениями.
+
+После получения длин сторон -использовать формулу Герона для вычисления площади треугольника.
+
+Чтобы жизнь не казалась мёдом, найдите формулу самостоятельно.
+
+После вычисления площади - вывести результат на консоль.*/
+    Console.WriteLine("Введите длины трех сторон треугольника:");
+    Console.WriteLine("Введите значение первой стороны:");
+    string? astr, bstr, cstr;
+    double a, b, c, p, s;
+    astr = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(astr)) Console.WriteLine("Значение не введено!");
+    else
+    {
+        Console.WriteLine($"Введено значение a: {astr}");
+        Console.WriteLine("Введите значение второй стороны:");
+        bstr = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(bstr)) Console.WriteLine("Значение не введено!");
+        else
+        {
+            Console.WriteLine($"Введено значение b: {bstr}");
+            Console.WriteLine("Введите значение третьей стороны:");
+            cstr = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(cstr)) Console.WriteLine("Значение не введено!");
+            else
+            {
+                Console.WriteLine($"Введено значение c: {cstr}");
+                a = Double.Parse(astr); b = Double.Parse(bstr); c = Double.Parse(cstr);
+                p = (a + b + c) / 2;
+                s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                Console.WriteLine($"Площадь треугольника S={s:f2}");
+            }
+        }
+    }
+}
 static void DZ1()
 {
     /*1. Запросить имя пользователя. Вывести Hello, [имя пользователя].
